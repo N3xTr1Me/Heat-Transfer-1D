50c38a1474f832744f87d029e31a17d4c9dc2265
N3xTr1Me <destriker17@gmail.com>
2023-02-10 13:56:18 +0300

    Analytical solution fix

--- configuration/config.py
+++ configuration/config.py
@@ -1,27 +1,28 @@
+from math import pi, exp, sin
 import numpy as np
 
 
 # space mesh conditions
 space_conditions = {
-    "interval": (0, 10),    # space partition interval borders
-    "steps": 100,          # number of elements in partition
+    "interval": (0, 1),    # space partition interval borders
+    "steps":50,          # number of elements in partition
     "step-size": None       # size of element on the mesh
 }
 
 # time mesh conditions
 time_conditions = {
-    "interval": (0, 5),    # time partition interval borders
-    "steps": 500,          # number of elements in partition
+    "interval": (0, 1),    # time partition interval borders
+    "steps": 10,          # number of elements in partition
     "step-size": None       # size of element on the mesh
 }
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 # explicit / implicit method switch
-is_explicit = True
+is_explicit = False
 
 # temperature on the borders of the rod
-t_border = 10
+t_border = 0
 
 # temperature inside the borders of the rod
 t_inside = 1
@@ -42,6 +43,13 @@ system_conditions = {
 
 
 # original function for analytical solution
-def u(x: float, t: float) -> float:
-    return t_border if x in [0, space_conditions["steps"] - 1] else t_inside
+def u(x: float, t: float, count = 1000) -> float:
+    result = 0
+    right_border = space_conditions["interval"][1]
+
+    for i in range(1, count + 1):
+        result += (t_inside * 2 * (1 - pow(-1, i)) / (i * pi)) \
+                  * sin(i * pi * x / right_border) * exp(-t * (i * pi / right_border) ** 2)
+
+    return result
 
